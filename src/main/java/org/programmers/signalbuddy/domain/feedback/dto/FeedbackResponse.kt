1a44435ce6f6c30ba16867cf774ee9a81eaff0eb
DongminL <milkskfk12@gmail.com>
2025-02-03 10:48:09 +0900

    fix: NoArgsConstructor 생성

--- src/main/java/org/programmers/signalbuddy/domain/feedback/dto/FeedbackResponse.kt
+++ src/main/java/org/programmers/signalbuddy/domain/feedback/dto/FeedbackResponse.kt
@@ -5,12 +5,12 @@ import org.programmers.signalbuddy.domain.member.dto.MemberResponse
 import java.time.LocalDateTime
 
 data class FeedbackResponse(
-    val feedbackId: Long,
-    val subject: String,
-    val content: String,
-    val likeCount: Long,
-    val answerStatus: AnswerStatus,
-    val createdAt: LocalDateTime,
-    val updatedAt: LocalDateTime,
-    val member: MemberResponse
+    val feedbackId: Long = 0L,
+    val subject: String = "",
+    val content: String = "",
+    val likeCount: Long = 0L,
+    val answerStatus: AnswerStatus = AnswerStatus.BEFORE,
+    val createdAt: LocalDateTime = LocalDateTime.now(),
+    val updatedAt: LocalDateTime = LocalDateTime.now(),
+    val member: MemberResponse = MemberResponse()
 )
--- src/main/java/org/programmers/signalbuddy/domain/member/dto/MemberResponse.kt
+++ src/main/java/org/programmers/signalbuddy/domain/member/dto/MemberResponse.kt
@@ -6,20 +6,20 @@ import org.programmers.signalbuddy.domain.member.entity.enums.MemberStatus
 
 data class MemberResponse(
     @Schema(description = "회원 ID", example = "1")
-    val memberId: Long,
+    val memberId: Long = 0L,
 
     @Schema(description = "회원 이메일", example = "test@example.com")
-    val email: String,
+    val email: String = "",
 
     @Schema(description = "회원 닉네임", example = "TestUser")
-    val nickname: String,
+    val nickname: String = "",
 
     @Schema(description = "프로필 이미지 URL", example = "http://example.com/profile.jpg")
-    val profileImageUrl: String,
+    val profileImageUrl: String = "",
 
     @Schema(description = "회원 역할", example = "USER")
-    val role: MemberRole,
+    val role: MemberRole = MemberRole.USER,
 
     @Schema(description = "회원 상태", exampleClasses = [MemberStatus::class])
-    val memberStatus: MemberStatus
+    val memberStatus: MemberStatus = MemberStatus.ACTIVITY
 )
